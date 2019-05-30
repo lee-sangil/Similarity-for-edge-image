24e4586ad1d6701d57dfc93e72d80338bb7b9cdf
Sangil Lee <aeternuslsi@gmail.com>
2019-05-30 20:27:42 +0900

    Minor change

--- calculateSimilarity.m
+++ calculateSimilarity.m
@@ -1,5 +1,7 @@
 function similarity = calculateSimilarity(refImage, estImage, RefCost, RefMatched, EstMatched)
 
+NumOfPixels = sum(sum(refImage | estImage));
+
 unmatchedRefImage = refImage > 0;
 unmatchedRefImage(RefMatched) = 0;
 unmatchedEstImage = estImage > 0;
@@ -11,4 +13,14 @@ unmatchedNumOfPixels = sum(sum(unmatchedRefImage | unmatchedEstImage));
 matchedCost = sum(sum(RefCost.*RefMatched));
 unmatchedCost = unmatchedNumOfPixels;
 
-similarity = 100 * (1 - (matchedCost+unmatchedCost)/(unmatchedNumOfPixels+matchedNumOfPixels));
+similarity = 100 * (1 - (matchedCost+unmatchedCost)/NumOfPixels);
+% similarity = 100 * (1 - (matchedCost+unmatchedCost)/(unmatchedNumOfPixels+matchedNumOfPixels));
+
+% red = unmatchedRefImage;
+% green = RefMatched | EstMatched;
+% blue = unmatchedEstImage;
+% img = cat(3, red,green,blue);
+% 
+% figure;imshow(refImage);
+% figure;imshow(double(img));
+% figure;imshow(estImage);
